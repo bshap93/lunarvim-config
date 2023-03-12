99e19bb43ec643a2717f791a1f129867b2cbf4e8
Benjamin Shapiro <bshap93@gmail.com>
2023-03-11 17:06:51 -0700

    Add plugins

--- config.lua
+++ config.lua
@@ -16,7 +16,7 @@ lvim.colorscheme = "default"
 -- lvim.use_icons = false
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
-lvim.colorscheme = "onenord"
+lvim.colorscheme = "tokyonight"
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
@@ -27,8 +27,8 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 --
 vim.opt.clipboard = "unnamedplus"
 vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
 -- override a default keymapping
@@ -59,13 +59,13 @@ vim.opt.shiftwidth = 2
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
-  name = "+Trouble",
-  r = { "<cmd>Trouble lsp_references<cr>", "References" },
-  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+    name = "+Trouble",
+    r = { "<cmd>Trouble lsp_references<cr>", "References" },
+    f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+    d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+    q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+    l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+    w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
 }
 
 -- TODO: User Config for predefined plugins
@@ -78,19 +78,19 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "yaml",
-  "dart",
-  "go",
+    "bash",
+    "c",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "typescript",
+    "tsx",
+    "css",
+    "rust",
+    "yaml",
+    "dart",
+    "go",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -176,74 +176,54 @@ lvim.lsp.automatic_servers_installation = true
 
 -- -- Additional Plugins
 lvim.plugins = {
-  { "rmehri01/onenord.nvim" },
-  { 'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim' },
-  {
-    "glepnir/lspsaga.nvim",
-    branch = "main",
-    config = function()
-      require("lspsaga").setup({
-        ui = {
-          code_action = "",
-        },
-      })
-    end,
-    requires = {
-      { "nvim-tree/nvim-web-devicons" },
-      --Please make sure you install markdown and markdown_inline parser
-      { "nvim-treesitter/nvim-treesitter" }
-    }
-  },
 
-
-  -- Dart plugins
-  -- { 'dart-lang/dart-vim-plugin' },
-  -- { 'thosakwe/vim-flutter' },
-  -- { 'natebosch/vim-lsc' },
-  -- { 'natebosch/vim-lsc-dart' },
-  -- Rust plugins
-  { 'timonv/vim-cargo' },
-
+    { "gbrlsnchs/winpick.nvim" },
+    { 'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim' },
+    {
+        "glepnir/lspsaga.nvim",
+        branch = "main",
+        config = function()
+            require("lspsaga").setup({
+                ui = {
+                    code_action = "", },
+            })
+        end,
+        requires = {
+            { "nvim-tree/nvim-web-devicons" },
+            --Please make sure you install markdown and markdown_inline parser
+            { "nvim-treesitter/nvim-treesitter" }
+        }
+    },
+    -- Git
+    { 'tpope/vim-fugitive' },
+    { 'vim-airline/vim-airline' },
+    -- Dart plugins
+    -- { 'dart-lang/dart-vim-plugin' },
+    { 'thosakwe/vim-flutter' },
+    -- { 'natebosch/vim-lsc' },
+    -- { 'natebosch/vim-lsc-dart' },
+    -- Rust plugins
+    { 'timonv/vim-cargo' },
+    { 'simrat39/rust-tools.nvim' },
 }
 
+vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true })
+
 local keymap = vim.keymap.set
+
+keymap({ "n", "v" }, "<leader>hf", "<cmd>Lspsaga code_action<CR>")
 
 keymap({ "n", "v" }, "<C-CR>", "<cmd>Lspsaga code_action<CR>")
 keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
 
 keymap("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>")
 
-
-
--- vim-plug Additional Plugins
--- Some plugins weren't working with Packer when I migrated
--- over to Lua
--- vim.cmd [[
---   call plug#begin()
-
-
---   "Dart/Flutter"
---   Plug 'dart-lang/dart-vim-plugin'
---   Plug 'natebosch/vim-lsc'
---   Plug 'natebosch/vim-lsc-dart'
---   Plug 'thosakwe/vim-flutter'
---   " Plug 'natebosch/vim-lsc-dart'
-
---   " initialize plugin syste  m
---   call plug#end()
--- ]]
-
--- Autocommands (https://neovim.io/doc/ser/autocmd.html)
--- vim.api.nvim_create_autocmd("BufEnter", {
---   pattern = { "*.json", "*.jpip install -r requirements.txtsonc" },
---   -- enable wrap mode for json files only
--- }  )
--- vim.api.nvim_create_autocmd("FileType", { dd
---   pattern = "zsh",
---     require("nvim-treesitter.highlight").attach(0, "bash")
---
 vim.cmd [[imap jj <Esc>]]
 vim.cmd [[imap kk <Esc>]]
+
+vim.cmd [[imap oo <Esc>o]]
+-- vim.cmd [[nnoremap <C-CR> <cmd>Lspsaga code_action<CR>]]
+
 vim.cmd [[nnoremap <TAB> i <TAB>]]
 vim.cmd [[map <leader> :w<kEnter>]]
 vim.cmd [[map <leader> <Esc>:w<cr>i]]
@@ -256,29 +236,3 @@ vim.cmd [[nnoremap <C-K> <C-W><C-K>]]
 vim.cmd [[nnoremap <C-L> <C-W><C-L>]]
 vim.cmd [[nnoremap <C-H> <C-W><C-H>]]
 vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
-
-
--- vim.cmd [[set splitbelow]]
--- vim.cmd [[set splitright]]
-
--- require("flutter-tools").setup {
---   debugger = {
---     enabled = true,
---     run_via_dap = true,
---   },
---   outline = { auto_open = false },
---   decorations = {
---     statusline = { device = true, app_version = true },
---   },
---   widget_guides = { enabled = true, debug = true },
---   dev_log = { enabled = false, open_cmd = "tabedit" },
---   lsp = {
---     settings = {
---       showTodos = true,
---       renameFilesWithClasses = "prompt",
---     },
---   },
-
--- }
--- require("telescope").load_extension "flutter"
